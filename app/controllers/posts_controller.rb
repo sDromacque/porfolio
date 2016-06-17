c6afeb5a77c9f86e7b0c6d99bae132e0194aed12
sebDromacque <sdromacque@clic2drive.com>
2016-06-17 14:00:44 +0200

    Create method for update Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,18 @@ class PostsController < ApplicationController
 
   end
 
+  def edit
+
+  end
+
+  def update
+    if @post.update post_params
+      redirect_to @post, notice: "Article update"
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def post_params
