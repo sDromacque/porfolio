02b0ebb06f26fb9ab7d90907c40cb1011c3712d2
sebDromacque <sdromacquedev@gmail.com>
2016-07-16 23:54:27 +0200

    fix get post & project welcome#home

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @posts = Post.order('Created_at').limit(3)
-    @projects = Project.order('Created_at').limit(3)
+    @posts = Post.order(created_at: 'desc').limit(3)
+    @projects = Project.order(created_at: 'desc').limit(3)
   end
 end
